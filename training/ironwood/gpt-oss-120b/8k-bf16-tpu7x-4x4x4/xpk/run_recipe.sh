9b5b24d4c1ab14df2478a38652f52ff2541d21bf
Carlos Bustamante Horta <carlosbus@google.com>
2026-09-02 07:39:21 +0000

    Update GPT-OSS 120b recipes on Ironwood

--- training/ironwood/gpt-oss-120b/8k-bf16-tpu7x-4x4x4/xpk/run_recipe.sh
+++ training/ironwood/gpt-oss-120b/8k-bf16-tpu7x-4x4x4/xpk/run_recipe.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # --- Environment Setup ---
-# This script requires uv and a Python 3.11 virtual environment with xpk installed.
+# This script requires uv and a Python 3.13 virtual environment with xpk installed.
 # If you haven't set up uv and the environment, please refer to the README.md.
 
 UV_VENV_PATH="${HOME}/.local/bin/venv"
-UV_PYTHON_VERSION="3.11"
+UV_PYTHON_VERSION="3.13"
 
 # Activate the virtual environment
 source "${UV_VENV_PATH}/bin/activate"
@@ -13,10 +13,13 @@ source "${UV_VENV_PATH}/bin/activate"
 # Check if xpk is installed in the venv
 if ! pip show xpk &> /dev/null; then
     echo "xpk not found in the virtual environment. Please install it by running:"
-    echo "pip install xpk==0.16.1"
+    echo "pip install xpk==1.11.0"
     exit 1
 fi
 # --- End Environment Setup ---
+
+set -e
+set -o pipefail
 
 # --- Configuration ---
 # Before running this script, please modify the environment variables below
@@ -28,13 +31,15 @@ export PROJECT_ID=""
 export CLUSTER_NAME=""
 export ZONE=""
 export BASE_OUTPUT_DIR=""
+export ARTIFACT_DIR=""
 export WORKLOAD_IMAGE=""
 export WORKLOAD_NAME="$(printf "%.26s" "${USER//_/-}-gpt-oss-120b")-$(date +%Y%m%d-%H%M)"
+
 
 # XLA Flags
 XLA_FLAGS=" \
   --xla_tpu_scoped_vmem_limit_kib=65536 \
-  --xla_tpu_impure_enable_packed_bf16_math_ops=true \
+  --xla_tpu_bf16_emission_mode=NATIVE_EMISSION \
   --xla_tpu_enable_sparse_core_reduce_scatter_v2=true \
   --xla_tpu_enable_sparse_core_collective_offload_all_gather=true \
   --xla_tpu_enable_sparse_core_collective_offload_2d_all_gather=true \
@@ -56,7 +61,9 @@ XLA_FLAGS=" \
   --xla_tpu_sparse_core_all_gather_latency_multiplier=1 \
   --xla_tpu_sparse_core_reduce_scatter_latency_multiplier=3 \
   --xla_tpu_enable_sparse_core_collective_aggregator=true \
-  --xla_tpu_enable_latency_hiding_layer_scheduler=true \
+  --xla_tpu_enable_latency_hiding_layer_scheduler=false \
+  --xla_tpu_rerun_latency_hiding_scheduler_post_sc_assignment=true \
+  --xla_tpu_enable_offloading_copy_to_sparsecore=false \
   --xla_tpu_scheduler_percent_shared_memory_limit=150 \
   --xla_tpu_enable_layer_scheduler_for_dependent_collectives=true \
   --xla_tpu_enable_sparse_core_collective_offload_nd_reduce_scatter=true \
@@ -76,14 +83,13 @@ profile_periodically_period=10000 \
 async_checkpointing=False \
 enable_checkpointing=False \
 use_custom_sort_vjp=True \
-fsdp_shard_on_exp=True \
+shard_exp_on_fsdp=True \
 use_tokamax_gmm=True \
 use_random_routing=True \
 remat_policy=custom \
 decoder_layer_input=offload \
 mlpwo=offload \
 opt_type=adamw \
-steps=20 \
 megablox=True \
 sparse_matmul=True \
 profiler=xplane \
@@ -108,10 +114,33 @@ ici_fsdp_transpose_parallelism=1 \
 ici_fsdp_parallelism=64 \
 ici_data_parallelism=2 \
 dataset_type=synthetic \
-dataset_path=gs://max-datasets-rogue \
+dataset_path=gs://<your_gcs_bucket> \
+use_gmm_v2=True \
+wi_tile_fwd_batch_seq=256 \
+wi_tile_fwd_embed_dim=2880 \
+wi_tile_fwd_mlp_dim=2944 \
+wi_tile_dlhs_batch_seq=256 \
+wi_tile_dlhs_mlp_dim=2880 \
+wi_tile_dlhs_embed_dim=2944 \
+wi_tile_drhs_batch_seq=512 \
+wi_tile_drhs_embed_dim=2880 \
+wi_tile_drhs_mlp_dim=1536 \
+wo_tile_fwd_batch_seq=256 \
+wo_tile_fwd_mlp_dim=2880 \
+wo_tile_fwd_embed_dim=2944 \
+wo_tile_dlhs_batch_seq=256 \
+wo_tile_dlhs_embed_dim=2880 \
+wo_tile_dlhs_mlp_dim=2944 \
+wo_tile_drhs_batch_seq=512 \
+wo_tile_drhs_mlp_dim=2880 \
+wo_tile_drhs_embed_dim=1536 \
+steps=20 \
 base_output_directory=${BASE_OUTPUT_DIR} \
 run_name=${WORKLOAD_NAME}"
 
+
+
+echo "=== Creating XPK Workload: $WORKLOAD_NAME ==="
 xpk workload create \
   --cluster=$CLUSTER_NAME \
   --project=$PROJECT_ID \
@@ -122,8 +151,15 @@ xpk workload create \
   --num-slices=1 \
   --docker-image="${WORKLOAD_IMAGE}" \
   --enable-debug-logs \
+   \
+   \
   --workload="${WORKLOAD_NAME}" \
-  --command="set -e && export ENABLE_PATHWAYS_PERSISTENCE='1' && \
+   \
+  --command="set -e && set -o pipefail && export ENABLE_PATHWAYS_PERSISTENCE='1' && \
 export LIBTPU_INIT_ARGS='${XLA_FLAGS}' && \
+export ARTIFACT_DIR='${ARTIFACT_DIR}' && \
 export JAX_PLATFORMS='tpu,cpu' && export ENABLE_PJRT_COMPATIBILITY='true' && \
-python3 -m MaxText.train MaxText/configs/base.yml ${MAXTEXT_ARGS}"
+ \
+ \
+python3 -m maxtext.trainers.pre_train.train maxtext/configs/base.yml ${MAXTEXT_ARGS} | tee train.log && \
+gcloud storage cp --no-user-output-enabled train.log ${ARTIFACT_DIR}/logs/train-\${TPU_WORKER_ID}.log"
